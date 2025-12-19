1681aaf77dfb3c48eb947e79e6248192fc2ce204
rachidnaitsaid10 <rachidnaitsaid10@gmail.com>
2025-12-19 15:55:39 +0100

    Update test unitaire 4

--- backend/Services/my_model.py
+++ backend/Services/my_model.py
@@ -2,7 +2,7 @@ import joblib
 import pandas as pd
 
 def predict(data):
-    model_path = '../../model/Model.pkl'
+    model_path = '../model/Model.pkl'
     model = joblib.load(model_path)
     pd_data = pd.DataFrame([data])
     if model.predict(pd_data)[0] == 1:
